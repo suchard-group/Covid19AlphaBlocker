1ebff5f3afbcfdc34a77455264e3483eea8ad623
Aki Nishimura <akihiko4@g.ucla.edu>
2020-07-07 14:54:58 -0700

    Update Rproj file name

--- replace_package_names.sh
+++ replace_package_names.sh
@@ -4,3 +4,4 @@ find . -type f \( -name "*.R" -o -name "*.Rd" -o -name "*.Rmd" \) -print0 | xarg
 additional_files="readme.md HydraConfig.json inst/settings/StudySpecification.json"
 sed -i '' -e 's/Covid19IncidenceRasInhibitors/Covid19IncidenceAlphaBlockers/g' $additional_files
 git mv man/Covid19IncidenceRasInhibitors.Rd man/Covid19IncidenceAlphaBlockers.Rd
+git mv Covid19IncidenceRasInhibitors.Rproj Covid19IncidenceAlphaBlockers.Rproj
